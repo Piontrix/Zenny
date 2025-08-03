28a591a5ed849b6a8372adde8d4a1f9c185d4ece
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-04 00:53:04 +0530

    Added Made By Piontrix Added

---
+++ Frontend/src/components/MadeWithLove.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MadeWithLove = () => {
+	return (
+		<div className="border-t border-roseclub-light mt-8 pt-6 text-center text-roseclub-accent">
+			MADE WITH ❤️ BY &nbsp;
+			<a
+				href="https://www.piontrix.com/"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-[1E2B3A] font-semibold hover:underline"
+			>
+				PIONTRIX
+			</a>
+		</div>
+	);
+};
+
+export default MadeWithLove;
